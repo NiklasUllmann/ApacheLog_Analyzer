e0341654261be36476f6743d67bdaeefc3c41ec6
Niklas Ullmann <niklas.ullmann@web.de>
2021-09-10 11:31:46 +0200

    new stuff

--- code/dataExtraction.py
+++ code/dataExtraction.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import re
 
 
 def getStatusCodeCount(df):
@@ -27,3 +28,47 @@ def getUsageHours(df):
     x.drop(['hour', 'minute'], axis=1, inplace=True)
 
     return x
+
+def getUsageDays(df):
+
+    x = df.groupby(['year', 'month','day']).size()
+    x = x.reset_index(name='counts')
+    x = x.reindex(index=range(0, len(x)))
+    d = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dez': 12}
+    x['month'] = x['month'].map(d)
+    x['date'] = x['year'].apply(str) + '-' + x['month'].apply(str) + '-' + x['day'].apply(str)
+    x = x.sort_values(by=['date'])
+    x.drop(['year', 'month', 'day'], axis=1, inplace=True)
+
+    return x
+
+
+def getRequestCount(df):
+
+    x = df.groupby("request").size()
+    x = x.reset_index(name='counts')
+    x = x.sort_values(by=['counts'], ascending=False)
+    x = x[x['request'].str.contains("GET")]
+    x = x.head(10)
+    x = x.sort_values(by=['counts'], ascending=True)
+    return x
+
+def getReferrer(df):
+
+    df['referrer'] = df['referrer'].apply(lambda x: _regex(x))
+
+    df = df.groupby("referrer").size()
+    x = df.reset_index(name='counts')
+    x = x.sort_values(by=['counts'], ascending=False)
+
+
+
+    print(x)
+
+
+def _regex(ref):
+    if ("www" in ref or "http" in ref):
+        re.findall
+        return re.findall('^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)', ref)[0]
+    else:
+        return ref
