26d92989add6bf5116c1785b1f8b9b0e8691a3ba
NiklasUllmann <niklas.ullmann@web.de>
2021-09-17 17:17:04 +0200

    cleaning

--- code/main.py
+++ code/main.py
@@ -25,8 +25,6 @@ from datetime import date, datetime
 from dash.exceptions import PreventUpdate
 
 
-
-
 app = dash.Dash("Apache SSL Log Analyzer & Dashboard",
                 external_stylesheets=[dbc.themes.DARKLY])
 
@@ -38,32 +36,9 @@ def main():
     df = loadLogFileToDF("../data/access_ssl_log")
 
     df = dataPreparation(df)
+    ref, rc, scc, tscc, uh, ud = getData(df)
 
-    ref = getReferrer(copy.deepcopy(df))
-
-    rc = getRequestCount(copy.deepcopy(df))
-    scc = getStatusCodeCount(copy.deepcopy(df))
-    tscc = getStatusCodeTimeLine(copy.deepcopy(df))
-    uh = getUsageHours(copy.deepcopy(df))
-    ud = getUsageDays(copy.deepcopy(df))
-
-    fig = px.pie(scc, values="count", names="status")
-    fig2 = px.line(tscc, x="date", y=tscc.columns)
-    fig3 = go.Figure(data=[
-        go.Bar(
-            x=uh['time'],
-            y=uh['counts']
-        ),
-        go.Scatter(
-            x=uh['time'],
-            y=uh['bestfit']
-        )
-
-    ])
-    fig3.update_layout(showlegend=False)
-    fig4 = px.bar(rc, x="counts", y="request", orientation="h")
-    fig5 = px.line(ud, x="date", y="counts")
-    fig6 = px.pie(ref, values="counts", names="referrer")
+    fig, fig2, fig3, fig4, fig5, fig6 = getFigs(ref, rc, scc, tscc, uh, ud)
 
     app.layout = html.Div(
         [
@@ -127,15 +102,14 @@ def main():
     )
 
     @app.callback(
-        [dash.dependencies.Output('pie-chart', 'figure'), 
-        dash.dependencies.Output('time-series', 'figure'),
-         dash.dependencies.Output('ug', 'figure'), 
-        dash.dependencies.Output('rc', 'figure'), 
-        dash.dependencies.Output('ud', 'figure'), 
-        dash.dependencies.Output('ref', 'figure'), ],
+        [dash.dependencies.Output('pie-chart', 'figure'),
+         dash.dependencies.Output('time-series', 'figure'),
+         dash.dependencies.Output('ug', 'figure'),
+         dash.dependencies.Output('rc', 'figure'),
+         dash.dependencies.Output('ud', 'figure'),
+         dash.dependencies.Output('ref', 'figure'), ],
         [dash.dependencies.Input('date-picker-range', 'start_date'),
          dash.dependencies.Input('date-picker-range', 'end_date')])
-
     def update_output(start_date, end_date):
         if start_date is not None and end_date is not None:
             x = copy.deepcopy(df)
@@ -146,36 +120,51 @@ def main():
                 str).apply(' '.join, 1), format='%Y %m %d')
             y = x[x['Datetime'].between(start, end)]
 
-            ref = getReferrer(copy.deepcopy(y))
-            rc = getRequestCount(copy.deepcopy(y))
-            scc = getStatusCodeCount(copy.deepcopy(y))
-            tscc = getStatusCodeTimeLine(copy.deepcopy(y))
-            uh = getUsageHours(copy.deepcopy(y))
-            ud = getUsageDays(copy.deepcopy(y))
+            ref, rc, scc, tscc, uh, ud = getData(y)
 
-            fig = px.pie(scc, values="count", names="status")
-            fig2 = px.line(tscc, x="date", y=tscc.columns)
-            fig3 = go.Figure(data=[
-                go.Bar(
-                    x=uh['time'],
-                    y=uh['counts']
-                ),
-                go.Scatter(
-                    x=uh['time'],
-                    y=uh['bestfit']
-                )
-
-            ])
-            fig3.update_layout(showlegend=False)
-            fig4 = px.bar(rc, x="counts", y="request", orientation="h")
-            fig5 = px.line(ud, x="date", y="counts")
-            fig6 = px.pie(ref, values="counts", names="referrer")
+            fig, fig2, fig3, fig4, fig5, fig6 = getFigs(ref, rc, scc, tscc, uh, ud)
 
             return fig, fig2, fig3, fig4, fig5, fig6
+
         else:
             raise PreventUpdate
 
     app.run_server(debug=False)
+
+
+def getData(df):
+
+    ref = getReferrer(copy.deepcopy(df))
+    rc = getRequestCount(copy.deepcopy(df))
+    scc = getStatusCodeCount(copy.deepcopy(df))
+    tscc = getStatusCodeTimeLine(copy.deepcopy(df))
+    uh = getUsageHours(copy.deepcopy(df))
+    ud = getUsageDays(copy.deepcopy(df))
+
+    return ref, rc, scc, tscc, uh, ud
+
+
+def getFigs(ref, rc, scc, tscc, uh, ud):
+
+    fig = px.pie(scc, values="count", names="status")
+    fig2 = px.line(tscc, x="date", y=tscc.columns)
+    fig3 = go.Figure(data=[
+        go.Bar(
+            x=uh['time'],
+            y=uh['counts']
+        ),
+        go.Scatter(
+            x=uh['time'],
+            y=uh['bestfit']
+        )
+
+    ])
+    fig3.update_layout(showlegend=False)
+    fig4 = px.bar(rc, x="counts", y="request", orientation="h")
+    fig5 = px.line(ud, x="date", y="counts")
+    fig6 = px.pie(ref, values="counts", names="referrer")
+
+    return fig, fig2, fig3, fig4, fig5, fig6
 
 
 if __name__ == "__main__":
